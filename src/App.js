476b5b930ae0451dfb9f70c85105480876090d8a
Peter-Roh <rmc2@naver.com>
2020-05-30 14:38:47 +0900

    learn reactjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
+class App extends React.Component {
+    state = {
+        isLoading: true,
+        movies: []
+    };
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({isLoading: false});
+        }, 5000);
+    }
+
+    render() {
+        const {isLoading} = this.state;
+
+        return <div>{isLoading ? "Loading..." : "We are ready!"}</div>;
+    }
 }
 
 export default App;
